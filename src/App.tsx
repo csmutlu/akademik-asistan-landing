1b25cf5d37d4f961327152503322d2ba040e5250
csmutlu <146027087+csmutlu@users.noreply.github.com>
2026-06-17 16:07:47 +0300

    feat: add FAQ, demo request form and compose the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 import { Header } from './sections/Header'
+import { Hero } from './sections/Hero'
+import { Problem } from './sections/Problem'
+import { Features } from './sections/Features'
+import { StudentStrip } from './sections/StudentStrip'
+import { Roles } from './sections/Roles'
+import { Pricing } from './sections/Pricing'
+import { Faq } from './sections/Faq'
+import { Contact } from './sections/Contact'
 import { Footer } from './sections/Footer'
 
 function App() {
@@ -8,7 +16,16 @@ function App() {
         İçeriğe geç
       </a>
       <Header />
-      <main id="main">{/* Bölümler Gün 3'te eklenecek */}</main>
+      <main id="main">
+        <Hero />
+        <Problem />
+        <Features />
+        <StudentStrip />
+        <Roles />
+        <Pricing />
+        <Faq />
+        <Contact />
+      </main>
       <Footer />
     </>
   )
